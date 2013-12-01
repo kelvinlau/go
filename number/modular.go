970dc9066f15d77325435e2b09e8ad6770e720c5
Kelvin Lau <kelvinlau0107@gmail.com>
2013-12-02 02:00:23 +0800

    Add GCD, ExGCD.

--- number/modular.go
+++ number/modular.go
@@ -17,3 +17,22 @@ func ModularPower(x, y, r int64) int64 {
 func ModularInvert(x, r int64) int64 {
 	return ModularPower(x, r-2, r)
 }
+
+// GCD returns the greatest common divisor of x and y.
+func GCD(x, y int64) int64 {
+	if y == 0 {
+		return x
+	}
+	return GCD(y, x%y)
+}
+
+// ExGCD returns a tuple (a, b, g) such that m * a + n * b == g and
+// g == GCD(m, n).
+func ExGCD(m, n int64) (a, b, g int64) {
+	if n == 0 {
+		return 1, 0, m
+	}
+	b, a, g = ExGCD(n, m%n)
+	b -= m / n * a
+	return
+}
--- number/modular_test.go
+++ number/modular_test.go
@@ -13,3 +13,29 @@ func TestModularInvert(t *testing.T) {
 		t.Fatalf("2^-1%11 should be %d, got %d.", 6, ModularInvert(2, 11))
 	}
 }
+
+func TestGCD(t *testing.T) {
+	test := func(x, y, e int64) {
+		if g := GCD(x, y); g != e {
+			t.Fatalf("GCD of %d %d should be %d, got %d.", x, y, e, g)
+		}
+	}
+	test(100, 26, 2)
+	test(1, 20, 1)
+	test(3, 4, 1)
+	test(15, 25, 5)
+}
+
+func TestExGCD(t *testing.T) {
+	test := func(x, y int64) {
+		a, b, g := ExGCD(x, y)
+		if a*x+b*y != g || g != GCD(x, y) {
+			t.Fatalf("ExGCD of %d %d got %d, %d, %d, calculated gcd = %d.",
+				x, y, a, b, g, a*x+b*y)
+		}
+	}
+	test(100, 26)
+	test(1, 20)
+	test(3, 4)
+	test(15, 25)
+}
