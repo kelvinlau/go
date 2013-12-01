cb9b30731b33e8ba11238c8831e1f2b28f30d7d1
Kelvin Lau <kelvinlau0107@gmail.com>
2013-12-02 01:25:20 +0800

    Add link-cut tree.

---
+++ lctree/lctree.go
@@ -0,0 +1,208 @@
+package lctree
+
+// Node is a node in the link-cut tree.
+type Node struct {
+	l, r, p, q *Node
+	z          bool
+	f, g, w    int
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func up(x *Node) {
+	l, r := x.l, x.r
+	x.g = x.w
+	if l != nil {
+		x.g = max(x.g, l.g+l.f)
+	}
+	if r != nil {
+		x.g = max(x.g, r.g+r.f)
+	}
+}
+
+func down(x *Node) *Node {
+	if x.z {
+		x.z = false
+		x.l, x.r = x.r, x.l
+		if x.l != nil {
+			x.l.z = !x.l.z
+		}
+		if x.r != nil {
+			x.r.z = !x.r.z
+		}
+	}
+	if x.f > 0 {
+		if x.l != nil {
+			x.l.f += x.f
+		}
+		if x.r != nil {
+			x.r.f += x.f
+		}
+		x.w += x.f
+		x.g += x.f
+		x.f = 0
+	}
+	return x
+}
+
+func push(x *Node) {
+	if x.p != nil {
+		push(x.p)
+	}
+	down(x)
+}
+
+func lr(x *Node) {
+	y := x.r
+	b := y.l
+	x.r = b
+	if b != nil {
+		b.p = x
+	}
+	y.p = x.p
+	if x.p != nil {
+		if x.p.l == x {
+			x.p.l = y
+		} else {
+			x.p.r = y
+		}
+	}
+	y.l = x
+	x.p = y
+	y.q = x.q
+	x.q = nil
+	up(x)
+}
+
+func rr(x *Node) {
+	y := x.l
+	b := y.r
+	x.l = b
+	if b != nil {
+		b.p = x
+	}
+	y.p = x.p
+	if x.p != nil {
+		if x.p.l == x {
+			x.p.l = y
+		} else {
+			x.p.r = y
+		}
+	}
+	y.r = x
+	x.p = y
+	y.q = x.q
+	x.q = nil
+	up(x)
+}
+
+func rot(x *Node) {
+	if x.p.l == x {
+		rr(x.p)
+	} else {
+		lr(x.p)
+	}
+}
+
+func splay(x *Node) {
+	push(x)
+	for x.p != nil {
+		p := x.p
+		if p.p == nil {
+			rot(x)
+		} else if (p.l == x) != (p.p.l == p) {
+			rot(x)
+			rot(x)
+		} else {
+			rot(p)
+			rot(x)
+		}
+	}
+	up(x)
+}
+
+func access(x *Node) {
+	var u, v *Node = x, nil
+	for u != nil {
+		splay(u)
+		if u.r != nil {
+			u.r.q = u
+			u.r.p = nil
+		}
+		u.r = v
+		if v != nil {
+			v.p = u
+			v.q = nil
+		}
+		up(u)
+		v = u
+		u = u.q
+	}
+	splay(x)
+}
+
+// Rotate makes x the root of the tree.
+func Rotate(x *Node) {
+	access(x)
+	x.z = !x.z
+}
+
+// Cut cuts out subtree of x from its parent.
+func Cut(x *Node) {
+	access(x)
+	if x.l != nil {
+		x.l.p = nil
+		x.l = nil
+		up(x)
+	}
+}
+
+// Link add an edge from u to v. v must be a root.
+func Link(u, v *Node) {
+	access(u)
+	access(v)
+	v.l = u
+	u.p = v
+	up(v)
+}
+
+// Root returns the root of x.
+func Root(x *Node) *Node {
+	access(x)
+	for down(x).l != nil {
+		x = x.l
+	}
+	return x
+}
+
+// Parent returns the parent of x.
+func Parent(x *Node) *Node {
+	access(x)
+	x = x.l
+	if x == nil {
+		return nil
+	}
+	for down(x).r != nil {
+		x = x.r
+	}
+	return x
+}
+
+// Add let weight += d for nodes on path from x to y.
+func Add(x, y *Node, d int) {
+	Rotate(x)
+	access(y)
+	y.f += d
+}
+
+// Query returns max weight of nodes on path from x to y.
+func Query(x, y *Node) int {
+	Rotate(x)
+	access(y)
+	return y.g
+}
---
+++ lctree/lctree_test.go
@@ -0,0 +1,75 @@
+package lctree
+
+import "testing"
+
+func TestLctree(t *testing.T) {
+	//   0      4
+	//   |      |
+	//   1      5
+	//  / \
+	// 2   3
+	nodes := [6]Node{}
+	a := func(i int) *Node {
+		return &nodes[i]
+	}
+	testEqual := func(a, b *Node) {
+		if a != b {
+			t.Fatalf("Expected %p, got %p.", a, b)
+		}
+	}
+	Link(a(0), a(1))
+	Link(a(1), a(2))
+	Link(a(1), a(3))
+	Link(a(4), a(5))
+	testEqual(Root(a(0)), a(0))
+	testEqual(Root(a(1)), a(0))
+	testEqual(Root(a(2)), a(0))
+	testEqual(Root(a(3)), a(0))
+	testEqual(Root(a(4)), a(4))
+	testEqual(Root(a(5)), a(4))
+	testEqual(Parent(a(0)), nil)
+	testEqual(Parent(a(1)), a(0))
+	testEqual(Parent(a(2)), a(1))
+	testEqual(Parent(a(3)), a(1))
+	testEqual(Parent(a(4)), nil)
+	testEqual(Parent(a(5)), a(4))
+
+	Cut(a(1))
+	Cut(a(5))
+	Rotate(a(2))
+	Link(a(0), a(5))
+	Link(a(0), a(4))
+	Link(a(0), a(2))
+	//   0
+	//  /|\
+	// 4 5 2
+	//     |
+	//     1
+	//     |
+	//     3
+	testEqual(Root(a(0)), a(0))
+	testEqual(Root(a(1)), a(0))
+	testEqual(Root(a(2)), a(0))
+	testEqual(Root(a(3)), a(0))
+	testEqual(Root(a(4)), a(0))
+	testEqual(Root(a(5)), a(0))
+	testEqual(Parent(a(0)), nil)
+	testEqual(Parent(a(1)), a(2))
+	testEqual(Parent(a(2)), a(0))
+	testEqual(Parent(a(3)), a(1))
+	testEqual(Parent(a(4)), a(0))
+	testEqual(Parent(a(5)), a(0))
+
+	testQuery := func(i, j, e int) {
+		if v := Query(a(i), a(j)); v != e {
+			t.Fatalf("Query(%d, %d) should be %d, got %d.", i, j, e, v)
+		}
+	}
+	Add(a(4), a(2), 1)
+	Add(a(5), a(1), 2)
+	Add(a(3), a(2), 4)
+	testQuery(4, 5, 3)
+	testQuery(4, 2, 7)
+	testQuery(3, 3, 4)
+	testQuery(0, 0, 3)
+}
