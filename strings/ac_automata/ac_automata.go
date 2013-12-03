b886e5cb24b56d91dc04f5feb2be8fa2781956a8
Kelvin Lau <kelvinlau0107@gmail.com>
2013-12-04 01:07:53 +0800

    Finish impl of ac_automata.

--- strings/ac_automata/ac_automata.go
+++ strings/ac_automata/ac_automata.go
@@ -6,17 +6,71 @@ type node struct {
 	word int
 }
 
+func newNode() *node {
+	return &node{next: make(map[rune]*node)}
+}
+
 // An AcAutomata is a DFA that surpports multi-string matching.
 type AcAutomata struct {
 	root *node
 }
 
-// Build builds an AcAutomata from s slice of strings.
-func Build(s []string) *AcAutomata {
-	return nil
+// New constructs an empty AcAutomata.
+func New() *AcAutomata {
+	return &AcAutomata{root: newNode()}
 }
 
-// Run reports how many string in the given set are substring of s.
+// Insert inserts a string into the automata.
+func (a *AcAutomata) Insert(s string) {
+	x := a.root
+	for _, z := range s {
+		if _, ok := x.next[z]; !ok {
+			x.next[z] = newNode()
+		}
+		x = x.next[z]
+	}
+	x.word++
+}
+
+// Build finalize the automata after inserting strings.
+func (a *AcAutomata) Build() {
+	q := []*node{a.root}
+	for len(q) > 0 {
+		u := q[0]
+		q = q[1:]
+		for z, v := range u.next {
+			x := u.fail
+			for x != nil && x.next[z] == nil {
+				x = x.fail
+			}
+			if x != nil {
+				v.fail = x.next[z]
+			} else {
+				v.fail = a.root
+			}
+			q = append(q, v)
+		}
+	}
+}
+
+// Run reports how many string in the given set are substring of s, can be run
+// only once.
 func (a *AcAutomata) Run(s string) int {
-	return 0
+	ans := 0
+	x := a.root
+	for _, z := range s {
+		for x != nil && x.next[z] == nil {
+			x = x.fail
+		}
+		if x != nil {
+			x = x.next[z]
+			for y := x; y != nil && y.word != -1; y = y.fail {
+				ans += y.word
+				y.word = -1
+			}
+		} else {
+			x = a.root
+		}
+	}
+	return ans
 }
---
+++ strings/ac_automata/ac_automata_test.go
@@ -0,0 +1,25 @@
+package ac_automata
+
+import "testing"
+
+func TestAcAutomata(t *testing.T) {
+	ss := []string{
+		"abc",
+		"abc",
+		"abd",
+		"xxx",
+		"xxxyy",
+		"xyzz",
+	}
+	f := "123abcdefxxxyzz"
+	e := 4
+
+	a := New()
+	for _, s := range ss {
+		a.Insert(s)
+	}
+	a.Build()
+	if g := a.Run(f); g != e {
+		t.Fatalf("Expected %d matches, got %d.", e, g)
+	}
+}
