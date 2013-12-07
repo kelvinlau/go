d01adba3a22b65a7e67c5673a6dcd7d9c734fb64
Kelvin Lau <kelvinlau0107@gmail.com>
2013-12-08 02:19:44 +0800

    Impl RelationCircle*.

--- geometry2d/circle.go
+++ geometry2d/circle.go
@@ -1,5 +1,11 @@
 package geometry2d
 
+// Circle with center and radius.
+type Circle struct {
+	Point
+	R float64
+}
+
 // InscribedCircleCenter returns the center of the max circle that fits into
 // the triangle abc.
 func InscribedCircleCenter(A, B, C Point) Point {
@@ -21,4 +27,21 @@ func CircumCircleCenter(A, B, C Point) Point {
 	c2 := (Sqr(a2) + Sqr(b2)) / 2
 	d := a1*b2 - a2*b1
 	return Point{A.X + (c1*b2-c2*b1)/d, A.Y + (a1*c2-a2*c1)/d}
+}
+
+// RelationCirclePoint returns 0 if p is on c, -1 if inside, +1 if outside.
+func RelationCirclePoint(c Circle, p Point) int {
+	return Sign(c.R - Dist(p, c.Point))
+}
+
+// RelationCircleLine returns 0 if l is tangent to c, -1 if intersected, +1 if
+// outside.
+func RelationCircleLine(c Circle, l Line) int {
+	return Sign(DistLinePoint(l, c.Point) - c.R)
+}
+
+// RelationCircleLineSeg returns 0 if l just touches c, -1 if inside of
+// intersected, +1 if outside.
+func RelationCircleLineSeg(c Circle, l LineSeg) int {
+	return Sign(DistLineSegPoint(l, c.Point) - c.R)
 }
