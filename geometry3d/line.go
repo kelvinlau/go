8419dc94a1547907c791d7d9300226792336c7be
Kelvin Lau <kelvinlau@google.com>
2013-12-09 17:40:39 +0800

    Add plane.

--- geometry3d/line.go
+++ geometry3d/line.go
@@ -9,6 +9,11 @@ type LineSeg struct {
 	P, Q Point
 }
 
+// LineVec is the vector of line.
+func LineVec(l LineSeg) Vector {
+	return Vec(l.P, l.Q)
+}
+
 // OnLineSeg reports if a is on l.
 func OnLineSeg(l LineSeg, a Point) bool {
 	ap := Vec(a, l.P)
@@ -21,8 +26,19 @@ func OnLineSeg(l LineSeg, a Point) bool {
 // -1: same plane, opposite side
 // 0:  otherwise
 func Side(l LineSeg, a, b Point) int {
-	v := Vec(l.P, l.Q)
+	v := LineVec(l)
 	pa := Vec(l.P, a)
 	pb := Vec(l.P, b)
 	return f.Sign(Dot(Cross(v, pa), Cross(v, pb)))
+}
+
+// Touched reports if l1, l2 share a common point.
+func Touched(l1, l2 LineSeg) bool {
+	v1 := LineVec(l1)
+	v2 := LineVec(l2)
+	if Zero(Cross(v1, v2)) {
+		return OnLineSeg(l1, l2.P) || OnLineSeg(l1, l2.Q) || OnLineSeg(l2, l1.P) || OnLineSeg(l2, l1.Q)
+	} else {
+		return Side(l1, l2.P, l2.Q) <= 0 && Side(l2, l1.P, l1.Q) <= 0
+	}
 }
---
+++ geometry3d/plane.go
@@ -0,0 +1,29 @@
+package geometry3d
+
+import (
+	f "github.com/kelvinlau/go/floats"
+)
+
+// Plane is a plane in 3d space, with normal vector N, passes point A.
+type Plane struct {
+	N Vector
+	A Point
+}
+
+// IntersectionPointPlaneSeg is the intersection point of a plane and a line
+// segment.
+func IntersectionPointPlaneSeg(e Plane, l LineSeg) (ok bool, ip Point) {
+	lhs := Dot(e.N, LineVec(l))
+	if f.Sign(lhs) == 0 {
+		return
+	}
+  v := Vec(l.P, e.A)
+	rhs := Dot(e.N, v)
+	t := rhs / lhs
+	if f.Sign(t) >= 0 && f.Sign2(t, 1) <= 0 {
+		ok = true
+		ip = Point(Add(Vector(l.P), Mul(v, t)))
+		return
+	}
+	return
+}
